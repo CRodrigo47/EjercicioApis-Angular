ded77b7493a04753bfacf3e9527270cebdf13226
ChristianRodrigo98 <christian.rodrigoalvarez@cieep.com>
2024-10-14 20:52:41 +0200

    Me falta un pelin de ejercicio de locos

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  imagenes = [{
+    url: "ValorantAPi.webp",
+    name: "Valorant",
+    path: "/valorant"
+  },
+  {
+    url: "MonsterHunterApi.webp",
+    name: "Monster Hunter",
+    path: "/mhw"
+  },
+  {
+    url: "PokemonApi.jfif",
+    name: "Pokemon TCG",
+    path: "/pokemon"
+  },
+  {
+    url: "Final_SpaceAPI.webp",
+    name: "Final Space",
+    path: "/finalSpace"
+  }
+  ]
 }
--- src/app/components/monster-hunter-list/monster-hunter-list.component.ts
+++ src/app/components/monster-hunter-list/monster-hunter-list.component.ts
@@ -35,4 +35,8 @@ export class MonsterHunterListComponent {
     )
   }
 
+  arrayRareza(rareza: number){
+    return Array(rareza)
+  }
+
 }
