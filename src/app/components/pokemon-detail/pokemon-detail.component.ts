984b2a241de78714f9ece53da679b369545bfe63
ChristianRodrigo98 <christian.rodrigoalvarez@cieep.com>
2024-10-27 15:46:38 +0100

    Ejercicio terminado

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -12,16 +12,38 @@ import { PokemonService } from '../../services/pokemon.service';
 export class PokemonDetailComponent implements OnInit {
   @Input('id') id!: number;
   card!: Daum;
+  bgCardColor!: string;
+  borderCardColor!: string;
   private pokeCardService: PokemonService = inject(PokemonService);
 
   ngOnInit(): void {
     this.loadCard();
+    
   }
 
   private loadCard() {
     this.pokeCardService.getCard(this.id).subscribe({
       next: (value) => {
         this.card = value.data;
+        if (["Metal", "Colorless", "Psychic"].includes(value.data.types[0])) {
+          this.bgCardColor = "bg-secondary bg-gradient text-black";
+          this.borderCardColor= "border-bottom border-black"
+        } else if (["Fire", "Fighting", "Fairy"].includes(value.data.types[0])) {
+          this.bgCardColor = "bg-danger bg-gradient text-black";
+          this.borderCardColor= "border-bottom border-black"
+        } else if (value.data.types[0] == "Grass") {
+          this.bgCardColor = "bg-success bg-gradient text-bg-success";
+          this.borderCardColor= "border-bottom"
+        } else if (value.data.types[0] == "Water") {
+          this.bgCardColor = "bg-info bg-gradient text-bg-info";
+          this.borderCardColor= "border-bottom border-black"
+        } else if (["Lightning", "Dragon"].includes(value.data.types[0])) {
+          this.bgCardColor = "bg-warning bg-gradient text-bg-warning";
+          this.borderCardColor= "border-bottom border-black"
+        } else if (value.data.types[0] == "Darkness") {
+          this.bgCardColor = "bg-dark bg-gradient text-bg-dark";
+          this.borderCardColor= "border-bottom"
+        }
       },
       error: (err) => console.log(err.message),
       complete: () => {console.log('Carta cargada con exito')}
